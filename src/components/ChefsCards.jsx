686514785e9e6eabe62b272c93c1cc420d150190
wahid9000 <wahidchowdhury9000@gmail.com>
2023-05-04 16:37:19 +0600

    lazyload updated

--- src/components/ChefsCards.jsx
+++ src/components/ChefsCards.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { FaBriefcase, FaCookie, FaThumbsUp } from 'react-icons/fa';
-import LazyLoad from 'react-lazyload';
+import LazyLoad from 'react-lazy-load';
 import { Link } from 'react-router-dom';
 
 const ChefsCards = ({ chef }) => {
@@ -8,8 +8,10 @@ const ChefsCards = ({ chef }) => {
     return (
         <div>
             <div className="card card-compact w-96 bg-base-100 shadow-xl">
+                <LazyLoad height={200}>
+                    <figure><img src={image} className='rounded-lg h-48 shadow-lg mt-5' alt="chef" /></figure>
+                </LazyLoad>
 
-               <LazyLoad><figure><img src={image} className='rounded-lg h-48 shadow-lg mt-5' alt="chef" /></figure></LazyLoad> 
 
 
                 <div className="card-body mt-2">
